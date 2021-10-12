8f5ca4dc972542890ca852a6e531207140223b68
Renan Rodrigues <renan_jacui@hotmail.com>
2021-10-12 18:32:16 -0300

    Client, TypeClient, Fones and Address

--- src/main/java/com/nelioalves/cursomc/CursoMcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursoMcApplication.java
@@ -1,9 +1,8 @@
 package com.nelioalves.cursomc;
 
-import com.nelioalves.cursomc.domain.Category;
-import com.nelioalves.cursomc.domain.Product;
-import com.nelioalves.cursomc.repositories.CategoryRepository;
-import com.nelioalves.cursomc.repositories.ProductRepository;
+import com.nelioalves.cursomc.domain.*;
+import com.nelioalves.cursomc.domain.enums.TypeClient;
+import com.nelioalves.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,6 +18,18 @@ public class CursoMcApplication implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private StateRepository stateRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoMcApplication.class, args);
@@ -43,5 +54,25 @@ public class CursoMcApplication implements CommandLineRunner {
 
         categoryRepository.saveAll(Arrays.asList(cat1, cat2));
         productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+        State sta1 = new State(null, "Minas Gerais");
+        State sta2 = new State(null, "Sao Paulo");
+
+        City city1 = new City(null, "Uberlandia", sta1);
+        City city2 = new City(null, "Sao Paulo", sta2);
+        City city3 = new City(null, "Campinas", sta2);
+
+        sta1.getCities().addAll(Arrays.asList(city1));
+        sta2.getCities().addAll(Arrays.asList(city2, city3));
+
+        stateRepository.saveAll(Arrays.asList(sta1,sta2));
+        cityRepository.saveAll(Arrays.asList(city1, city2, city3));
+
+        Client cli1 = new Client(null, "Maria Silva", "maria@gmail.com", "36378912377", TypeClient.PHYSICALPERSON.getCod());
+        Address a1 = new Address(null, "Rua Flores", "300", "Apt 203", "Jardim", "38220834", cli1, city1);
+        Address a2 = new Address(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, city2);
+
+        clientRepository.saveAll(Arrays.asList(cli1));
+        addressRepository.saveAll(Arrays.asList(a1, a2));
     }
 }
